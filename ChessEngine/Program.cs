3b196e86e21eced218544c2cb0140fa56631bef4
rtrydev <r.kulka44@gmail.com>
2021-09-08 19:46:11 +0200

    clean main

--- ChessEngine/Program.cs
+++ ChessEngine/Program.cs
@@ -11,19 +11,8 @@ namespace ChessEngine
         static void Main(string[] args)
         {
             var handler = new GameHandler();
-            var lines = File.ReadAllLines("/home/rtry/testperft");
-            int iterator = 1;
-            for (int i = 0; i < lines.Length; i++)
-            {
-                var separated = lines[i].Split(",");
-                handler = new GameHandler();
-                var value = handler.CalculateNodeCount(separated[0], null, 5, false);
-                if (value == Convert.ToInt32(separated[5])) Console.WriteLine($"OK {iterator++}");
-                else
-                {
-                    Console.WriteLine($"FAIL {iterator++} {Convert.ToInt32(separated[5])} vs {value}");
-                }
-            }
+            handler.InitializeGame();
+            handler.StartGame();
 
         }
     }
